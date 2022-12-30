7685fdc43e14266b29f2cf33bdd25c07176b11b2
imNaval <nehranvl2017@gmail.com>
2022-12-31 00:17:16 +0530

    build level-1 security

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,20 +11,68 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+// connect to database
+const url = "mongodb://127.0.0.1:27017/userDB";
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+//schema
+userSchema = {
+  email: String,
+  password: String
+}
+const User = mongoose.model("User", userSchema);
+
 
 app.get("/", function(req, res){
   res.render("home");
 });
 
-app.get("/login", function(req, res){
-  res.render("login");
-});
 
-app.get("/register", function(req, res){
-  res.render("register");
-});
+///////register route /////////////
+app.route("/register")
+  .get(function(req, res){
+    res.render("register");
+  })
+  .post(function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+      })
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else {
+        res.render("secrets")
+      }
+    });
+  });
 
+/////////////login route //////////////
+app.route("/login")
+  .get(function(req, res){
+    res.render("login");
+  })
+  .post(function(req, res){
+    email = req.body.username;
+    password = req.body.password;
 
+    User.findOne({email: email}, function(err, foundUser){
+      if(err){
+        console.log(err);
+      }else {
+        if(foundUser){
+          if (foundUser.password === password) {
+            res.render("secrets");
+          }else {
+            res.send("password not match")
+          }
+        }else {
+          res.send("user not found");
+        }
+      }
+    })
+  });
 
 
 
