8fd50229a6099ee91778a118f494e2ccbf78679c
imNaval <nehranvl2017@gmail.com>
2022-12-31 18:56:49 +0530

    Security lvl-5, Add passport and cookies

--- app.js
+++ app.js
@@ -8,15 +8,29 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encypt = require("mongoose-encryption");
 // const md5 = require("md5");
-const bcrypt = require("bcrypt");
-
-const saltRounds = 7;
+// const bcrypt = require("bcrypt");
+// const saltRounds = 7;
+//
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+//it must be here
+app.use(session({
+  secret: 'Our little secret.',
+  resave: false,
+  saveUninitialized: false
+  // cookie: { secure: true }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // connect to database
 const url = "mongodb://127.0.0.1:27017/userDB";
@@ -29,14 +43,32 @@ userSchema = new mongoose.Schema({
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 //encryption
 //-->see in .env
 // userSchema.plugin(encypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = mongoose.model("User", userSchema);
 
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+//
 app.get("/", function(req, res){
   res.render("home");
+});
+
+////secrets
+app.get("/secrets", function(req, res){
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }else {
+    res.redirect("/login");
+  }
 });
 
 
@@ -46,22 +78,19 @@ app.route("/register")
     res.render("register");
   })
   .post(function(req, res){
-    //Technique 2 (auto-gen a salt and hash):
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        // Store hash in your password DB.
-        const newUser = new User({
-            email: req.body.username,
-            password: hash //md5(req.body.password)
-          })
-        newUser.save(function(err){
-          if(err){
-            console.log(err);
-          }else {
-            res.render("secrets")
-          }
-        });
-    });
+
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+      if(err){
+        console.log(err);
+        res.redirect("/register");
+      }else {
+        passport.authenticate("local")(req, res, function(){
+          res.redirect("/secrets");
+        })
+      }
+    })
   });
+
 
 /////////////login route //////////////
 app.route("/login")
@@ -69,32 +98,37 @@ app.route("/login")
     res.render("login");
   })
   .post(function(req, res){
-    email = req.body.username;
-    password = req.body.password;
 
-    User.findOne({email: email}, function(err, foundUser){
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    })
+
+    req.login(user, function(err){
       if(err){
         console.log(err);
       }else {
-        if(foundUser){
-          bcrypt.compare(password, foundUser.password, function(err, result) {
-            if(result === true){
-              res.render("secrets")
-            }else {
-              res.send("password not match");
-            }
-          });
-        }else {
-          res.send("user not found");
-        }
+        passport.authenticate("local")(req,res, function(){
+          res.redirect("/secrets");
+        })
       }
     })
   });
 
 
+/////////logout//////////////
+app.get("/logout", function(req, res){
+  req.logout(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  })
+});
 
 
 
+
+//listen
 app.listen(3000, function(){
   console.log("server started on port 3000");
 });
